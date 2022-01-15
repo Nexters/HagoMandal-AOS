77a30bd071b0f93e203b34de0e985dcf31a59617
greedy0110 <greedy0110@gmail.com>
2022-01-15 16:39:49 +0900

    very first commit - setting very default dependencies.
    - Compose
    - Coroutine
    - Hilt
    - Room
    - Retrofit
    - OkHttp3
    - Navigation
    - Moshi

---
+++ app/src/main/java/com/greedy0110/hagomandal/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.greedy0110.hagomandal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.greedy0110.hagomandal.ui.theme.HagoMandalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HagoMandalTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HagoMandalTheme {
+        Greeting("Android")
+    }
+}
