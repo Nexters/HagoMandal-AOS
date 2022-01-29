57f52c4a6426dfef73218ed821eb0bffc5850d05
nylah-j <nylah.j@kakaoent.com>
2022-01-29 14:11:56 +0900

    add timber, hilt 설정

---
+++ app/src/main/java/com/greedy0110/hagomandal/ui/HagoMandalApplication.kt
@@ -0,0 +1,13 @@
+package com.greedy0110.hagomandal.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class HagoMandalApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
