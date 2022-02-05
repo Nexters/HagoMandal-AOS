61ddee123b08cbbc6fc4864e60d64db37dd5a2f1
greedy0110 <greedy0110@gmail.com>
2022-02-05 22:43:27 +0900

    SubGoalCardList selected에 따라서 화면 조정하기

--- app/src/main/java/com/greedy0110/hagomandal/ui/SubGoalCardList.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/SubGoalCardList.kt
@@ -1,7 +1,15 @@
 package com.greedy0110.hagomandal.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
@@ -10,7 +18,52 @@ import androidx.compose.ui.unit.dp
 import com.greedy0110.hagomandal.ui.theme.HagoMandalTheme
 
 @Composable
-fun SubGoalLayout(
+fun SubGoalCardList() {
+    val cardColors = listOf(
+        Color(0xff5533c0),
+        Color(0xff35ace4),
+        Color(0xff09c6a6),
+        Color(0xfff3c403),
+    )
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    @Composable
+    fun getCard(color: Color, index: Int) {
+        SubGoalCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { selectedIndex = index },
+            backgroundColor = color
+        )
+    }
+
+    Column {
+        SubGoalLayout(modifier = Modifier) {
+            cardColors.subList(0, selectedIndex + 1)
+                .forEachIndexed { index, color -> getCard(color, index) }
+        }
+        if (selectedIndex != cardColors.lastIndex) {
+            Spacer(modifier = Modifier.size(10.dp))
+            SubGoalLayout(modifier = Modifier) {
+                cardColors.subList(selectedIndex + 1, cardColors.size)
+                    .forEachIndexed { index, color ->
+                        getCard(color, index + selectedIndex + 1)
+                    }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSubGoalCardList() {
+    HagoMandalTheme {
+        SubGoalCardList()
+    }
+}
+
+@Composable
+private fun SubGoalLayout(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
@@ -24,8 +77,10 @@ fun SubGoalLayout(
             measurable.measure(constraints)
         }
 
-        // TODO: height를 미리 계산할 수 있잖아?
-        layout(constraints.maxWidth, constraints.maxHeight) {
+        val layoutHeight = ((placeables.size - 1) * gap.roundToPx() + placeables.last().height)
+            .coerceAtMost(constraints.maxHeight)
+
+        layout(constraints.maxWidth, layoutHeight) {
             var yPosition = 0
 
             placeables.forEach { placeable ->
@@ -33,23 +88,5 @@ fun SubGoalLayout(
                 yPosition += gap.roundToPx()
             }
         }
-    }
-}
-
-@Composable
-fun SubGoalCardList() {
-    SubGoalLayout(modifier = Modifier) {
-        SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff5533c0))
-        SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff35ace4))
-        SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff09c6a6))
-        SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xfff3c403))
-    }
-}
-
-@Preview
-@Composable
-fun PreviewSubGoalCardList() {
-    HagoMandalTheme {
-        SubGoalCardList()
     }
 }
--- app/src/main/java/com/greedy0110/hagomandal/ui/SubGoalScreen.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/SubGoalScreen.kt
@@ -2,14 +2,12 @@ package com.greedy0110.hagomandal.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.greedy0110.hagomandal.ui.theme.HagoMandalTheme
@@ -29,12 +27,7 @@ fun SubGaolScreen() {
             Spacer(modifier = Modifier.size(4.dp))
             Text(text = mainGoal)
             Spacer(modifier = Modifier.size(22.dp))
-            Column(modifier = Modifier) {
-                SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff5533c0))
-                SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff35ace4))
-                SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xff09c6a6))
-                SubGoalCard(modifier = Modifier.fillMaxWidth(), backgroundColor = Color(0xfff3c403))
-            }
+            SubGoalCardList()
         }
     }
 }
