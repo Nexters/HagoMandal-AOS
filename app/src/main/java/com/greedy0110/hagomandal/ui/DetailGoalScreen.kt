b89b99046a1bb45f413bed66b4c8640a818ea81d
RoadtoS7 <kshvv975@gmail.com>
2022-02-06 19:14:02 +0900

    [FEAT] DetailGoalScreen(실천목표) 펼쳐지기 애니메이션 구현

--- app/src/main/java/com/greedy0110/hagomandal/ui/DetailGoalScreen.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/DetailGoalScreen.kt
@@ -2,11 +2,21 @@ package com.greedy0110.hagomandal.ui
 
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
