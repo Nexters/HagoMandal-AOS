c8910bd50ae8a7fa6f84dbe2f68d223d2c6dfc28
greedy0110 <greedy0110@gmail.com>
2022-02-10 21:23:18 +0900

    ic check 추가

--- app/src/main/java/com/greedy0110/hagomandal/ui/subgoal/ColorChooser.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/subgoal/ColorChooser.kt
@@ -4,18 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.greedy0110.hagomandal.R
 import com.greedy0110.hagomandal.ui.theme.HagoMandalTheme
 import com.greedy0110.hagomandal.util.`if`
 
@@ -48,13 +53,21 @@ fun ColorChooserUnit(
         modifier = modifier
             .size(28.dp)
             .`if`(selected) {
-                // TODO: 은은히 나오는 컬러는 무엇? 명확히 알아야할 듯
                 background(cardColorPairs[index][0].copy(alpha = 0.4f), CircleShape)
-                // background(Color.Black, CircleShape)
             }
             .padding(4.dp)
             .background(cardColorBrushes[index], CircleShape)
     ) {
+        if (selected) {
+            Icon(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(),
+                painter = painterResource(id = R.drawable.ic_check),
+                contentDescription = null,
+                tint = Color.Black
+            )
+        }
     }
 }
 
