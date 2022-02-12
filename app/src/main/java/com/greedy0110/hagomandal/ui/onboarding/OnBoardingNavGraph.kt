0baef0af0f3843a51c845fdb9e7e79c8ec001b0e
greedy0110 <greedy0110@gmail.com>
2022-02-12 16:50:20 +0900

    Onboarding Process IntroScreen 만듬

--- app/src/main/java/com/greedy0110/hagomandal/ui/onboarding/OnBoardingNavGraph.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/onboarding/OnBoardingNavGraph.kt
@@ -1,52 +1,41 @@
 package com.greedy0110.hagomandal.ui.onboarding
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.greedy0110.hagomandal.R
 
 object OnBoardingDestinations {
     const val INTRO = "intro"
-    const val DISPLAY = "display"
-    const val NICKNAME = "nickname"
-    const val DEADLINE = "deadline"
-    const val GOAL_TYPE = "goal_type"
-    const val MAIN_GOAL = "main_goal"
+    const val MANDALART_GUIDE = "mandarlart_guide"
+    const val NAME = "name"
+    const val TYPE = "type"
+    const val CAREER = "career"
+    const val OTHER_JOB = "other_job"
+    const val GET_STARTED = "get_started"
 }
-
-const val DISPLAY_RES_ID = "res_id"
 
 @Composable
 fun OnBoardingNavGraph(
     navController: NavHostController = rememberNavController(),
     startDestination: String = OnBoardingDestinations.INTRO
 ) {
-
-    // TODO: 테스트용
-    val startDestination = OnBoardingDestinations.NICKNAME
-
     NavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(
-            route = "${OnBoardingDestinations.DISPLAY}/{$DISPLAY_RES_ID}",
-            arguments = listOf(navArgument(DISPLAY_RES_ID) { type = NavType.IntType })
-        ) { backStackEntry ->
-            backStackEntry.arguments
-                ?.getInt(DISPLAY_RES_ID, -1)
-                .takeIf { it != -1 }
-                .also { resId -> DisplayScreen(resId ?: R.drawable.ic_launcher_foreground) }
-        }
-        composable(OnBoardingDestinations.NICKNAME) {
-            NicknameScreen()
-        }
-        composable(OnBoardingDestinations.DEADLINE) {
-            NicknameScreen()
+        composable(OnBoardingDestinations.INTRO) {
+            IntroScreen(
+                onWhatIsMandalartClick = {
+                    Unit
+                },
+                onNext = {
+                    Unit
+                }
+            )
         }
     }
 }
--- buildSrc/src/main/kotlin/com/greedy0110/hagomandal/Dep.kt
+++ buildSrc/src/main/kotlin/com/greedy0110/hagomandal/Dep.kt
@@ -72,6 +72,7 @@ object Dep {
         const val navigation = "androidx.navigation:navigation-compose:2.5.0-alpha01"
         const val activity = "androidx.activity:activity-compose:1.4.0"
         const val mdc3 = "androidx.compose.material3:material3:1.0.0-alpha02"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0"
     }
 
     object Accompanist {
