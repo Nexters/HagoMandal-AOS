bd37b94360330a76365fca8d8936f0511a1086ef
greedy0110 <greedy0110@gmail.com>
2022-02-20 12:40:04 +0900

    ShareTopBar 화면 작업

--- app/src/main/java/com/greedy0110/hagomandal/ui/share/ShareTopBar.kt
+++ app/src/main/java/com/greedy0110/hagomandal/ui/share/ShareTopBar.kt
@@ -2,9 +2,12 @@ package com.greedy0110.hagomandal.ui.share
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,15 +15,20 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.greedy0110.hagomandal.R
 import com.greedy0110.hagomandal.ui.theme.HagoMandalTheme
 import com.greedy0110.hagomandal.ui.theme.t12
+import com.greedy0110.hagomandal.ui.theme.t18
 import com.greedy0110.hagomandal.util.coloredShadow
 
 class ShareAction(
@@ -29,16 +37,58 @@ class ShareAction(
     val action: () -> Unit = {},
 )
 
+val actions = listOf(
+    ShareAction(iconRes = R.drawable.ic_user, title = "이름 수정"),
+    ShareAction(iconRes = R.drawable.ic_edit_2, title = "목표 수정"),
+    ShareAction(iconRes = R.drawable.ic_trash, title = "목표 삭제"),
+    ShareAction(iconRes = R.drawable.ic_image, title = "이미지로 저장"),
+)
+
 @Composable
 fun ShareTopBar(
-    title: String,
-    actions: List<ShareAction>
+    modifier: Modifier = Modifier,
+    title: String = "",
+    actions: List<ShareAction> = emptyList(),
+    expanded: MutableState<Boolean> = remember { mutableStateOf(false) }
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Transparent),
+        horizontalAlignment = Alignment.End
+    ) {
+        Row(Modifier.fillMaxWidth()) {
+            Text(
+                text = title,
+                style = t18,
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center
+            )
+            Icon(
+                modifier = Modifier
+                    .clickable { expanded.value = expanded.value.not() },
+                painter = painterResource(id = R.drawable.ic_more_vertical),
+                contentDescription = null,
+                tint = Color.White,
+            )
+        }
+        if (expanded.value) {
+            Spacer(modifier = Modifier.height(12.dp))
+            ShareTopBarDropDown(actions = actions)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewShareTopBar() {
+    HagoMandalTheme {
+        ShareTopBar(
+            title = "D-53",
+            actions = actions,
+            expanded = remember { mutableStateOf(true) },
+        )
+    }
 }
 
 @Composable
@@ -48,6 +98,7 @@ fun ShareTopBarDropDown(
 ) {
     Column(
         modifier = modifier
+            .width(140.dp)
             .coloredShadow(
                 color = Color.Black,
                 alpha = 0.16f,
@@ -86,13 +137,6 @@ fun ShareTopBarDropDown(
 @Preview
 @Composable
 fun PreviewShareTopBarDropDown() {
-    val actions = listOf(
-        ShareAction(iconRes = R.drawable.ic_user, title = "이름 수정"),
-        ShareAction(iconRes = R.drawable.ic_edit_2, title = "목표 수정"),
-        ShareAction(iconRes = R.drawable.ic_trash, title = "목표 삭제"),
-        ShareAction(iconRes = R.drawable.ic_image, title = "이미지로 저장"),
-    )
-
     HagoMandalTheme {
         ShareTopBarDropDown(actions = actions)
     }
