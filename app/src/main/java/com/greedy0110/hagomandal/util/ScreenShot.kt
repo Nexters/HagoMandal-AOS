c33812eb88641665727739c840726d84b45517ac
greedy0110 <greedy0110@gmail.com>
2022-03-12 21:17:25 +0900

    Q 이상 버전에서 이미지 저장 완료

--- app/src/main/java/com/greedy0110/hagomandal/util/ScreenShot.kt
+++ app/src/main/java/com/greedy0110/hagomandal/util/ScreenShot.kt
@@ -1,10 +1,14 @@
 package com.greedy0110.hagomandal.util
 
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
 import android.view.View
+import androidx.annotation.RequiresApi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -50,15 +54,11 @@ suspend fun Bitmap.saveAsImage(
 
     withContext(ioDispatcher) {
         try {
-            val externalDir: File = context.getExternalFilesDir(null)
-                ?: throw IOException("can't save because external dir is not available right now.")
-            val filename = getRandomFileName("jpg")
-            val file = File(externalDir, filename)
-
-            FileOutputStream(file).use { outStream ->
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                saveAsImageInMediaStore(bitmap, context)
+            } else {
+                TODO("뭐 해야하지?")
             }
-            Timber.d("beanbean saved > ${file.absolutePath}")
         } catch (e: Throwable) {
             e.printStackTrace()
         }
@@ -74,4 +74,48 @@ private fun isExternalStorageWritable(): Boolean {
 private fun isExternalStorageReadable(): Boolean {
     return Environment.getExternalStorageState() in
         setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
+}
+
+private fun saveAsImageInExternalDir(bitmap: Bitmap, context: Context) {
+    val externalDir: File = context.getExternalFilesDir(null)
+        ?: throw IOException("can't save because external dir is not available right now.")
+    val filename = getRandomFileName("jpg")
+    val file = File(externalDir, filename)
+
+    FileOutputStream(file).use { outStream ->
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream)
+    }
+    Timber.d("beanbean saved > ${file.absolutePath}")
+}
+
+// https://codechacha.com/ko/android-mediastore-insert-media-files/
+@RequiresApi(Build.VERSION_CODES.Q)
+private fun saveAsImageInMediaStore(bitmap: Bitmap, context: Context) {
+    val contentResolver =
+        context.contentResolver ?: throw IllegalStateException("contentResolver can't be null")
+    val filename = getRandomFileName("jpg")
+
+    val imageValues = ContentValues().apply {
+        // TODO: 상대 경로 지정하자.
+        put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        put(MediaStore.Images.Media.IS_PENDING, 1)
+    }
+
+    val collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    val item = contentResolver.insert(collection, imageValues)
+        ?: throw IOException("error while inserting a image values")
+
+    val imageFileDescriptor = contentResolver.openFileDescriptor(item, "w", null)
+        ?: throw IOException("can't open file descriptor")
+
+    imageFileDescriptor.use {
+        FileOutputStream(it.fileDescriptor).use { outStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream)
+        }
+    }
+
+    imageValues.clear()
+    imageValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+    contentResolver.update(item, imageValues, null, null)
 }
